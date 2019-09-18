24c5af335ad55595d0e8f5a3e609512ec84ac112
suba0107 <suba0107@stud.kea.dk>
2019-09-18 15:09:36 +0200

    kodekommentarer

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ document.addEventListener("DOMContentLoaded", hentJson);
 
 async function hentJson() {
     const minJson = await fetch("https://spreadsheets.google.com/feeds/list/1Y6c_YOI5XmIz6zh6OT_943mOkupPzIyyYaDALWxi6LQ/od6/public/values?alt=json&fbclid=IwAR3GvdQvKcjYnqT18MlJN99RDpNzlUmAW68sfh6Vl2ChXgjJi_lKYb9sou8");
-    sanger = await minJson.json();
+    sanger = await minJson.json(); /*Henter google sheet ind som jSon asynkront*/
     visSangListen();
     if (window.location.href.indexOf('spotify') < 0) {
-
+        /*Hvis url IKKE indeholder 'spotify' kald disse funktioner*/
 
         klikFilter();
         clickonFilterknap();
@@ -33,6 +33,7 @@ function clickonFilterknap() {
 
 function toggleFilterlist() {
     document.querySelector("#menu").classList.toggle("hidden");
+    /*Toggle filterknap*/
 }
 
 function tilbageKlik() {
@@ -41,17 +42,21 @@ function tilbageKlik() {
 }
 
 function tilbageForside() {
-    window.history.back();
+    window.history.back(); /*Gå tilbage til forrige side*/
 }
 
 function visSangListen() {
 
     sanger.feed.entry.forEach(sang => {
-        if ((sang.gsx$bruger.$t == bruger || "alle" == getBruger) && (filterbruger == "alle" || filterbruger == sang.gsx$brugernavn.$t)) {
-            document.querySelector(".brugerplaylist").textContent = `${sang.gsx$brugernavn.$t} Playlist`
+        /*For hver sang*/
+        let getBrugerNavn = urlParams.get("brugernavn"); /*Tag værdien af "brugernavn" fra url*/
+        if (sang.gsx$bruger.$t == bruger || "alle" == getBruger) {
+            /*Hvis sangens bruger-id er det samme som bruger i url ELLER hvis brugeren i url er "alle" */
+            document.querySelector(".brugerplaylist").textContent = getBrugerNavn; /*Skriv brugernavn fra url ud i .brugerplaylist*/
 
             if (filtergenre == "alle" || filtergenre == sang.gsx$genre.$t) {
-                let klon = sangTemplate.cloneNode(true).content;
+                /*Hvis der er trykket på "alle" ELLER en bestemt genre*/
+                let klon = sangTemplate.cloneNode(true).content; /*Klon koden i html for hver sang, der forekommer*/
                 klon.querySelector("img").src = "albums-billeder/" + sang.gsx$albumcover.$t + "-album.jpg";
                 klon.querySelector("h2").textContent = sang.gsx$navn.$t;
                 klon.querySelector(".kunstner").textContent = `af ${sang.gsx$kunstner.$t}`;
@@ -60,8 +65,9 @@ function visSangListen() {
                 klon.querySelector("#spille-knap").src = "assets/spille-knap.svg";
                 sangListe.appendChild(klon);
                 sangListe.lastElementChild.addEventListener("click", () => {
+                    /*Tilføj eventlistener på det sidste barn af listen*/
 
-                    location.href = `single.html?uri=${sang.gsx$uri.$t}`;
+                    location.href = `single.html?uri=${sang.gsx$uri.$t}`; /*Når der klikkes, åbn single.html med denne sti*/
                 });
             }
 
@@ -78,22 +84,25 @@ function klikFilter() {
 }
 
 function filtreringGenre() {
-    filtergenre = this.dataset.genre;
-    document.querySelector(".filtretnavn").textContent = this.textContent;
+    filtergenre = this.dataset.genre; /*tag værdien af dataatribute med genre på den knap, der bliver trykket på*/
+    document.querySelector(".filtretnavn").textContent = this.textContent; /*Udskriv navnet i overskrift*/
 
     document.querySelectorAll(".filter").forEach(elm => {
-        elm.classList.remove("valgt");
+        elm.classList.remove("valgt"); /*Fjern klassen "avalgt fra alle knapper*/
     });
 
-    this.classList.add("valgt");
+    this.classList.add("valgt"); /*Tilføj klassen "valgt" på knappen, der er trykket på*/
 
-    sangListe.innerHTML = "";
+    sangListe.innerHTML = ""; /*nulstil*/
     visSangListen();
 }
 
 function visLyrics() {
 
     if (window.location.href.indexOf('single.html') > 0) {
+        /*Hvis url indeholder "single.html" fra https://www.encodedna.com/javascript/check-if-url-contains-a-given-string-using-javascript.htm
+         */
+        /*IndexOf vil give et tal for hver gang teksten forekommer. Hvis den ikke forekommer vil den give -1*/
         document.querySelector(".back").addEventListener("click", () => {
             history.back();
         })
