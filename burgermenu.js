34956a64ce337ae0f4cc36cbf0609352e00df7cf
suba0107 <suba0107@stud.kea.dk>
2019-09-19 10:27:35 +0200

    Kommentarer i burgermenu

--- burgermenu.js
+++ burgermenu.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu); /*Klik eventlistener på burgermenuen*/
 }
 
 
 
 function toggleMenu() {
     console.log("toggleMenu");
-    document.querySelector("#burgermenu").classList.toggle("hide");
+    document.querySelector("#burgermenu").classList.toggle("hide"); /*Toggle på menuen, så den åbner og lukker, når man klikker på den*/
 
-    let erSkjult = document.querySelector("#burgermenu").classList.contains("hide");
+    let erSkjult = document.querySelector("#burgermenu").classList.contains("hide"); /*Varibel, når menuen indeholder "hide" som klasse*/
 
     if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent = "☰";
+        document.querySelector("#menuknap").textContent = "☰"; /*Hvis menuen indeholder classen "skjult" sæt tegnet til at være dette*/
     } else {
-        document.querySelector("#menuknap").textContent = "X";
+        document.querySelector("#menuknap").textContent = "X"; /*Ellers skift tegn til X*/
     }
 }
